9549064fdbf6cd31e5353ee5341820b173de1714
ddimaanovv <slam-m57@yandex.ru>
2025-04-03 00:00:23 +0300

    Еще одно решение

--- src/index.tsx
+++ src/index.tsx
@@ -3,13 +3,14 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
+import MainComponent from "./App2";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <MainComponent />
   </React.StrictMode>
 );
 
